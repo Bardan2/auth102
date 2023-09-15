6494c5bb53c9b9dd0e09342d23a42f9d70fb7ffd
Bardan <nepalibardan@gmail.com>
2023-09-15 23:51:27 +0545

    add login

--- app.js
+++ app.js
@@ -22,18 +22,59 @@ app.post("/register", async (req, res) => {
   const username = req.body.username;
   const password = req.body.password;
 
-  if (!email || !username || !password) {
-    return res.send("Please provide email, username, password");
-  }
+  // aako ko email ko kohi xa ki nae find garnu paryo
 
-  // let's store datas on databse...
-  await users.create({
-    email: email,
-    username: username,
-    password: bcrypt.hashSync(password, 8),
+  const emailExist = await users.findAll({
+    where: {
+      email: email,
+    },
+  });
+  if (emailExist.length > 0) {
+    res.send("User with that email already registered");
+  } else {
+    if (!email || !username || !password) {
+      return res.send("Please provide email, username, password");
+    }
+
+    // let's store datas on databse...
+    await users.create({
+      email: email,
+      username: username,
+      password: bcrypt.hashSync(password, 8),
+    });
+
+    res.redirect("/login");
+  }
+});
+
+// Login...
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
+app.post("/login", async (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  // 1st - tyo registered vayeko email ko kohi hamro users table ma xa ki xaina
+  const userExists = await users.findAll({
+    where: {
+      email: email,
+    },
   });
 
-  res.send("User Register is successfull.");
+  if (userExists.length > 0) {
+    // 2nd -> password check garnu paryo
+    const isMatch = bcrypt.compareSync(password, userExists[0].password);
+    if (isMatch) {
+      res.send("Logged in Successfully");
+    } else {
+      res.send("Invalid Email or Password");
+    }
+  } else {
+    //
+    res.send("Invalid Email Or Password");
+  }
 });
 
 app.listen("3000", function () {
